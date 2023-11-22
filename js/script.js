1cc460c52deff1648dd6fd75283aab4a6fd8f0be
asafcastigliano <asafcastigliano@gmail.com>
2023-11-22 16:47:47 +0100

    finito esercizio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let num = 0;
+
+for(let i=0; i<100; i++){
+
+    num++;
+
+    if(num % 5 == 0 && num % 3 == 0){
+        console.log("FizzBuzz");
+    } else if (num % 5 == 0){
+        console.log("Fizz");
+    } else if (num % 3 == 0){
+        console.log("Buzz");
+    } else {
+        console.log(num);
+    }
+    
+}
